a9227e6782a46fd4002e4ab3e83a729a18ce6f00
wali55 <walisantunu@gmail.com>
2023-02-03 19:57:38 +0600

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,34 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({ id, image, title, description, tags, source, visit }) => (
+        <BlogCard key={id}>
+          <img style={{width: '100%', height: '100%', borderTopLeftRadius: "10px", borderTopRightRadius: "10px"}} src={image} alt="project_image" />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent style={{marginTop: '15px'}}>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks target='_blank' href={visit}>Visit</ExternalLinks>
+            <ExternalLinks target='_blank' href={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,46 @@
 export const projects = [
-  {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 0,
-  },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
-];
-
-export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
-];
+    {
+      title: 'Dental Clinic App',
+      description: "Built a Full Stack MERN Application Using React, Node.js, Express & MongoDB. This App is called Doctors Portal and it is a simple web app that allows users to create account, book an appointment for a particular treatment and also pay the bill using credit card.",
+        image: '/images/1.png',
+        tags: ['Mongo', 'Express', 'React', 'Node'],
+      source: 'https://github.com/wali55/doctors-portal-client',
+      visit: 'https://doctors-portal-77390.web.app/',
+      id: 0,
+    },
+    {
+      title: 'Metaverse Website',
+      description:"Built a modern responsive website using Framer Motion, Tailwind CSS and Next.js 13. It is called Metaversus. It is a beautifully designed and animated landing page based on the concept of Metaverse.",
+      image: '/images/2.png',
+      tags: ['React', 'JavaScript', 'Next.js'],
+      source: 'https://github.com/wali55/metaversus',
+      visit: 'https://metaversus-wali.netlify.app/',
+      id: 1,
+    },
+    {
+      title: 'Dentist Website',
+      description: "It is a website built for a dentist for his personal chamber. It is called Smile. User can create account then login. After logged in users can go to the appointment page to fill up the form and make an appointment.",
+        image: '/images/3.png',
+        tags: ['React', 'Firebase', 'Tailwind CSS'],
+      source: 'https://github.com/wali55/assignment-ten',
+      visit: 'https://assignment-ten-ef009.web.app/',
+      id: 2,
+    },
+    {
+      title: 'Website for Review',
+      description: "This is single page application built using react router. It is called Best Laptop Deals. User can go to different routes without reloading the page.",
+      image: '/images/4.png',
+      tags: ['React', 'React router', 'Tailwind CSS'],
+      source: 'https://github.com/wali55/assignment-nine',
+      visit: 'https://assignment-nine-wali.netlify.app/',
+      id: 3,
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: 'Started my journey', },
+    { year: 2018, text: 'Worked as a freelance developer', },
+    { year: 2019, text: 'Founded JavaScript Mastery', },
+    { year: 2020, text: 'Shared my projects with the world', },
+    { year: 2021, text: 'Started my own platform', },
+  ];
