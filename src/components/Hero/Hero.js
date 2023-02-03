ca24a10fe20e5d07ef3bd8e0dae992d2564bb995
wali55 <walisantunu@gmail.com>
2023-02-03 16:50:18 +0600

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        I'm Mohammad Waliullah <br />
+        A Front-End Developer 
+      </SectionTitle>
+      <SectionText>
+        I develop beautiful websites and web applications and I love what I do.
+      </SectionText>
+      <Button onClick={() => window.open('http://google.com','_blank')}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
